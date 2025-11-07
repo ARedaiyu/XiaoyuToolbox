c79edf6d15b234a0fe4b6221ff36c1d6ca402afc
Redaiyu <107354183+ARedaiyu@users.noreply.github.com>
2025-11-08 01:49:53 +0800

    完善《随机密码生成器》的界面

--- XiaoyuToolbox/Views/RandomTool/RandomPasswordView.xaml.cs
+++ XiaoyuToolbox/Views/RandomTool/RandomPasswordView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
 
 namespace XiaoyuToolbox.Views.RandomTool;
 
@@ -8,4 +9,21 @@ public partial class RandomPasswordView : UserControl
     {
         InitializeComponent();
     }
+}
+
+public partial class RandomPasswordViewModel : ObservableObject
+{
+    [ObservableProperty] private bool uppercasesIncluded = true;
+    [ObservableProperty] private bool lowercasesIncluded = true;
+    [ObservableProperty] private bool numbersIncluded = true;
+
+    [ObservableProperty] private bool specialCharactersIncluded;
+    [ObservableProperty] private bool hasExclusion = true;
+
+    [ObservableProperty] private string specialCharacters;
+    [ObservableProperty] private string exclusionCharacters;
+    [ObservableProperty] private string passwordLengthText;
+    [ObservableProperty] private string passwordNumberText;
+
+    [ObservableProperty] private string passwords;
 }
